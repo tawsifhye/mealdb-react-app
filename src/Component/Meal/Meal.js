a36fece31a75f4d1e73253cacaa8b64c9c5a9e75
tawsifhye <tawsif.lucse@gmail.com>
2021-09-25 20:56:29 +0600

    data loaded to UI

--- src/Component/Meal/Meal.js
+++ src/Component/Meal/Meal.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Meal.css'
+import './Meal.css'
 
 const Meal = (props) => {
-    // console.log(props);
-    const {strCategory} = props.meal;
+    console.log(props.meal);
+    const {strCategory, strCategoryThumb} = props.meal;
     // console.log(strCategory);
     return (
         <div className="meal">
+            <img src={strCategoryThumb} alt="" />
             <h4>{strCategory}</h4>
         </div>
     );
--- src/Component/Meals/Meals.js
+++ src/Component/Meals/Meals.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Meal from '../Meal/Meal';
+import './Meals.css'
 
 const Meals = () => {
     const [meals, setMeals] = useState([]);
@@ -13,9 +14,12 @@ const Meals = () => {
     // console.log(meals);
     return (
         <div>
-            {
+            <h2 style={{textAlign: 'center', color: 'white', marginTop: 0}}>Meals by Category</h2>
+            <div className='meals'>
+                {
                 meals.map( meal => <Meal meal={meal}></Meal>)
-            }
+                }
+            </div>
         </div>
     );
 };
