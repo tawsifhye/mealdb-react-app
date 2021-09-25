bdd8d9735463ca8a7a123bc8e22b8c0e609a4ba2
tawsifhye <tawsif.lucse@gmail.com>
2021-09-25 22:25:39 +0600

    font awsome icon added

--- src/Component/Meal/Meal.js
+++ src/Component/Meal/Meal.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import './Meal.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
 import './Meal.css'
 
 const Meal = (props) => {
     console.log(props.meal);
     const {strCategory, strCategoryThumb} = props.meal;
+    const btnIcon = <FontAwesomeIcon icon={faDownload} />
     // console.log(strCategory);
     return (
         <div className="meal">
             <img src={strCategoryThumb} alt="" />
             <h4>{strCategory}</h4>
+            <button>{btnIcon}Add Item</button>
         </div>
     );
 };
